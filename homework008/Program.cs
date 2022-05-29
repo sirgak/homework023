318ac3726c379c45636d95d61cf40087f5f263d4
sirgak <life_4_ever@mail.ru>
2022-05-29 09:10:17 +0300

    test
    text

---
+++ homework008/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homework008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int step = 7;
+            int max = 100;
+
+            for (firstNumber = 5; firstNumber < max; firstNumber += step)
+            //for так как легче задать условие для последовательности цифр.
+            {
+                Console.WriteLine(firstNumber);
+            }
+        }
+        }       
+    }
